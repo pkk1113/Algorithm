f1ca1c9264fad387a840033da30b524db6957e3f
park-keon-kuk <pkk1113@naver.com>
2019-09-07 13:31:47 +0900

    카카오 2019 - 1차 시험 헤더 미리 작성

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,1 +1,9 @@
-﻿#include "pr_42899.h"
+﻿// 여기에 헤더파일 하나만 넣어 주세요.
+
+#include "k19_1.h"
+//#include "k19_2.h"
+//#include "k19_3.h"
+//#include "k19_4.h"
+//#include "k19_5.h"
+//#include "k19_6.h"
+//#include "k19_7.h"
