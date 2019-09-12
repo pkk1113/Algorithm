6722a76dbb236cd3b47bd35d7361e8be3c7e2531
park-keon-kuk <pkk1113@naver.com>
2019-09-13 02:49:28 +0900

    백준 쉬운 계산 수 : 성공

---
+++ Algorithm/bj_10844.h
@@ -0,0 +1,31 @@
+// 쉬운 계단 수
+
+#ifdef _WIN32
+#pragma warning(disable:4996) // c io
+#pragma warning(disable:6031) // 반환값 사용 안 함
+#pragma warning(disable:4430) // 반환값 사용 안 함
+#endif
+
+#include <vector>
+using namespace std;
+
+main() {
+	int N;
+	scanf("%d", &N);
+	int mem[100][10] = { 0, };
+
+	for (int s = 0; s <= 9; s++)
+		mem[0][s] = 1;
+
+	for (int len = 1; len < N; len++)
+		for (int s = 0; s <= 9; s++)
+			mem[len][s] = (((s > 0) ? mem[len - 1][s - 1] : 0)
+						   + ((s < 9) ? mem[len - 1][s + 1] : 0)) % 1000000000;
+	int ans = 0;
+	for (int s = 1; s <= 9; s++) {
+		ans += mem[N - 1][s];
+		ans %= 1000000000;
+	}
+
+	printf("%d\n", ans);
+}
