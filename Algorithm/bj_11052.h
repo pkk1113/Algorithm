bb847e9efcc9a6994c4e783769735856541301eb
park-keon-kuk <pkk1113@naver.com>
2019-09-13 07:36:30 +0900

    .

---
+++ Algorithm/bj_11052.h
@@ -0,0 +1,14 @@
+// 카드 구매하기
+
+#ifdef _WIN32
+#pragma warning(disable:4996) // c io
+#pragma warning(disable:6031) // 반환값 사용 안 함
+#endif
+
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	printf("hi");
+}
