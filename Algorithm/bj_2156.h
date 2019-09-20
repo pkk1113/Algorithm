02d15181ad1fc17f5ca5d3e9b0d402c0ed920b2b
park-keon-kuk <pkk1113@naver.com>
2019-09-20 12:58:30 +0900

    백준 포도주 마시기: 케이스를 잘 생각해보자

--- Algorithm/bj_2156.h
+++ Algorithm/bj_2156.h
@@ -12,5 +12,26 @@
 using namespace std;
 
 int main() {
+	int n;
+	scanf("%d", &n);
+	auto arr = vector<int>(4);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+	}
 
+	auto mem = vector<int>(n + 1, 0);
+	int c[3]; // cases
+	for (int i = 1; i <= n; i++) {
+		int mem_2 = i >= 2 ? mem[i - 2] : 0;
+		int mem_3 = i >= 3 ? mem[i - 3] : 0;
+
+		// cases
+		c[0] = mem[i - 1];
+		c[1] = arr[i] + mem_2;
+		c[2] = arr[i] + arr[i - 1] + mem_3;
+
+		mem[i] = *max_element(c, c + 3);
+	}
+
+	printf("%d\n", mem[n]);
 }
